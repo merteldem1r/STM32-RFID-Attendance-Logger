d3490bc338efce5ccca157b6bf7712bedb32ff65
ahsenyenisey <ahsenyeuisei@gmail.com>
2025-04-30 15:22:38 +0300

    first commit

--- Serial-Comms/main.py
+++ Serial-Comms/main.py
@@ -1,1 +1,13 @@
-print("STM32 RFID Device")
+import serial
+import time
+
+ser = serial.Serial('/dev/tty.debug-console')
+ser.baudrate = 115200
+
+print("Serial at port: ", ser.name)
+
+while True:
+    val = ser.readline()
+    val_str = str(val, "UTF-8")
+    print(val_str)
+    time.sleep(3)
