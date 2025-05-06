500751659a742f795f564f219a94210aa68fb8bd
Mert Eldemir <mert1greatest@gmail.com>
2025-05-06 04:52:03 +0300

    STM32 RFID & MFRC522 Library changed and implemented for HAL Library

---
+++ STM32-RFID/Core/Inc/defines.h
@@ -0,0 +1,17 @@
+// All global #defines for that project
+// Created By: Mert Eldemir
+// GitHub: https://github.com/merteldem1r
+
+// RC522 MODULE DEFINES
+
+/* Default SPI used for*/
+#define MFRC522_SPI 					&hspi1
+
+/* Default CS (Chip Select & SDA Pin) pin used */
+#define MFRC522_CS_PORT                 GPIOD
+#define MFRC522_CS_PIN                  GPIO_PIN_6
+
+// LCD DEFINES
+
+// MAIN LOGIC DEFINES
+
