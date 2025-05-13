2f088d3f8ecdaecca576bdef76793e354ea7b60f
ahsenyenisey <ahsenyeuisei@gmail.com>
2025-05-13 04:33:03 +0300

    sth changed

--- Serial-Server/main.py
+++ Serial-Server/main.py
@@ -3,9 +3,13 @@ from utils import csv_util as CSV_UTIL
 import time
 import threading
 
-
+#Arduino test
 ser = serial.Serial("/dev/tty.usbserial-10")
 ser.baudrate = 9600
+
+#STM32
+# ser = serial.Serial("/dev/tty.usbserial-B0044FJ3")
+# ser.baudrate = 115200
 print("Serial PORT: ", ser.name)
 
 # 1- when the programme first run check if db excel file exist (if not create db excel file)
--- Serial-Server/utils/attendance_lists_util.py
+++ Serial-Server/utils/attendance_lists_util.py
@@ -5,7 +5,7 @@ import os
 time_in_secs = time.time()
 # current_time_full_day = time.ctime(time_in_secs)
 current_day = time.strftime("%d", time.gmtime(time_in_secs))
-current_month = time.strftime("%b", time.gmtime(time_in_secs))
+current_month = time.strftime("%m", time.gmtime(time_in_secs))
 current_year = time.strftime("%Y", time.gmtime(time_in_secs))
 
 full_date = f"{current_day}" + "-" + f"{current_month}" + "-" + f"{current_year}"
@@ -26,14 +26,14 @@ def update_attendance_list(card_uid_str: str, user_name, user_id):
     df = pd.read_csv(PATH_AL, sep=",", engine="python")
     df = df.dropna(how="all")
 
-    new_time = time.ctime(time.time())
+    new_time = time.strftime("%d.%m.%Y %I:%M %p", time.localtime())
     
     if card_uid_str not in df["card_uid"].values:
         new_row = pd.DataFrame([{
             "card_uid": card_uid_str,
             "user_name": f"{user_name}",
             "user_id": f"{user_id}",
-            "last_log_date": current_time_full_day, 
+            "last_log_date": new_time, 
             "total_reads": 0
         }])
         df = pd.concat([df, new_row], ignore_index=True)
